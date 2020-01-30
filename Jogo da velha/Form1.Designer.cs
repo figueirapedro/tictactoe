c3f514005134957fb39cfb4fcb67f68fb12ad2c8
Pedro Figueira <pedro.figueirab@hotmail.com>
2020-01-30 15:03:12 -0300

    v1

---
+++ Jogo da velha/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Jogo_da_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
+            this.txtjogada = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(167, 38);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(107, 103);
+            this.btn11.TabIndex = 0;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(288, 38);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(107, 103);
+            this.btn12.TabIndex = 1;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Location = new System.Drawing.Point(167, 166);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(107, 103);
+            this.btn21.TabIndex = 3;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.btn21_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Location = new System.Drawing.Point(288, 166);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(107, 103);
+            this.btn22.TabIndex = 4;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.btn22_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Location = new System.Drawing.Point(411, 38);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(107, 103);
+            this.btn13.TabIndex = 5;
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.btn13_Click);
+            // 
+            // btn23
+            // 
+            this.btn23.Location = new System.Drawing.Point(411, 166);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(107, 103);
+            this.btn23.TabIndex = 6;
+            this.btn23.UseVisualStyleBackColor = true;
+            this.btn23.Click += new System.EventHandler(this.btn23_Click);
+            // 
+            // btn31
+            // 
+            this.btn31.Location = new System.Drawing.Point(167, 291);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(107, 103);
+            this.btn31.TabIndex = 7;
+            this.btn31.UseVisualStyleBackColor = true;
+            this.btn31.Click += new System.EventHandler(this.btn31_Click);
+            // 
+            // btn32
+            // 
+            this.btn32.Location = new System.Drawing.Point(288, 291);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(107, 103);
+            this.btn32.TabIndex = 8;
+            this.btn32.UseVisualStyleBackColor = true;
+            this.btn32.Click += new System.EventHandler(this.btn32_Click);
+            // 
+            // btn33
+            // 
+            this.btn33.Location = new System.Drawing.Point(411, 291);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(107, 103);
+            this.btn33.TabIndex = 9;
+            this.btn33.UseVisualStyleBackColor = true;
+            this.btn33.Click += new System.EventHandler(this.btn33_Click);
+            // 
+            // txtjogada
+            // 
+            this.txtjogada.AutoSize = true;
+            this.txtjogada.Location = new System.Drawing.Point(49, 211);
+            this.txtjogada.Name = "txtjogada";
+            this.txtjogada.Size = new System.Drawing.Size(62, 13);
+            this.txtjogada.TabIndex = 10;
+            this.txtjogada.Text = "Nada ainda";
+            this.txtjogada.Click += new System.EventHandler(this.txtjogada_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 435);
+            this.Controls.Add(this.txtjogada);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn23);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn31;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn33;
+        private System.Windows.Forms.Label txtjogada;
+    }
+}
+
---
+++ Jogo da velha/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_velha
+{
+    public partial class Form1 : Form
+    {
+        bool acabou = false;
+        int jogada = 0;
+
+
+        static void statusDOjogo(ref bool acabou, Button l11, Button l21, Button l31, Button l12, Button l22, Button l32, Button l13, Button l23, Button l33)
+        {
+            if (l11.Text != "" || l12.Text != "" || l13.Text != "" || l21.Text != "" || l22.Text != "" || l23.Text != "" || l31.Text != "" || l32.Text != "" || l31.Text != "")
+            {
+
+                #region Coluna 1
+                if (((l11.Text == "X") && (l21.Text == "X") && (l21.Text == "X") && (l31.Text == "X") ||
+                    (l11.Text == "O") && (l21.Text == "O") && (l21.Text == "O") && (l31.Text == "O")))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Coluna 2
+
+
+                if ((l12.Text == "X" && l22.Text == "X" && l32.Text == "X") || (l12.Text == "O" && l22.Text == "O" && l32.Text == "O"))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Coluna 3
+
+
+                if ((l13.Text == "O" && l23.Text == "O" && l33.Text == "O") || (l13.Text == "X" &&  l23.Text == "X" && l33.Text == "X"))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Diagonal ED
+
+
+                if ((l11.Text == "O"  && l22.Text == "O" && l33.Text == "O") || (l11.Text == "X" && l22.Text == "X" && l33.Text == "X"))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Diagonal DE
+
+
+                if ((l13.Text == "X" && l22.Text == "X" && l31.Text == "X") || (l13.Text == "O" && l22.Text == "O" && l31.Text == "O"))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Linha 1
+                if ((l11.Text == "X" && l12.Text == "X" && l13.Text == "X") || (l11.Text == "O" && l12.Text == "O" && l13.Text == "O"))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Linha 2
+                if ((l21.Text == "X" && l22.Text == "X" && l23.Text == "X") || (l21.Text == "O" && l22.Text == "O" && l23.Text == "O"))
+                {
+                    acabou = true;
+                }
+                #endregion
+
+                #region Linha 3
+                if ((l31.Text == "X" && l32.Text == "X" && l33.Text == "X") || (l31.Text == "O" && l32.Text == "O" && l33.Text == "O"))
+                {
+                    acabou = true;
+                }
+                #endregion
+            }
+            else
+            {
+                acabou = false;
+            }
+        }
+
+        static void XouO(ref int jogada, Button btn)
+        {
+            if (jogada % 2 == 0)
+            {
+                btn.Text = "O";
+                btn.Enabled = false;
+            }
+            else
+            {
+                btn.Text = "X";
+                btn.Enabled = false;
+            }
+
+            jogada++;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+
+            XouO(ref jogada, btn11);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+        }
+
+        private void btn21_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn21);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)       
+                MessageBox.Show("Fim de jogo!");
+        }
+        
+        private void btn31_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn31);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+        
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn12);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+
+        private void btn22_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn22);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+
+        private void btn32_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn32);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn13);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+
+        private void btn23_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn23);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+
+        private void btn33_Click(object sender, EventArgs e)
+        {
+            XouO(ref jogada, btn33);
+            statusDOjogo(ref acabou, btn11, btn21, btn31, btn12, btn22, btn32, btn13, btn23, btn33);
+            if (acabou == true)
+                MessageBox.Show("Fim de jogo!");
+        }
+
+        private void txtjogada_Click(object sender, EventArgs e)
+        {
+
+            string xd = Convert.ToString(jogada);
+
+            txtjogada.Text = xd;
+
+            if (jogada%2 == 0)
+            {
+                txtjogada.Text = "É a vez do 'O' jogar!";
+            }
+            else
+            {
+                txtjogada.Text = "É a vez do 'X' jogar!";
+            }
+
+            if (acabou == true)
+            {
+                txtjogada.Text = "Fim de Jogo!";
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+
+            string xd = Convert.ToString(jogada);
+
+            txtjogada.Text = xd;
+
+            if (jogada % 2 == 0)
+            {
+                txtjogada.Text = "É a vez do 'O' jogar!";
+            }
+            else
+            {
+                txtjogada.Text = "É a vez do 'X' jogar!";
+            }
+
+            if (acabou == true)
+            {
+                txtjogada.Text = "Fim de Jogo!";
+            }
+        }
+    }
+}
